2de4256524c841a6464b308434321241702e936d
gardenunez <gardenunez@gmail.com>
2014-05-01 17:39:16 +0200

    Decouple funcionalities and add some constants.

--- JiraClient/Program.cs
+++ JiraClient/Program.cs
@@ -15,9 +15,13 @@ namespace JiraClient
         static void Main(string[] args)
         {
             List<string> sources = new List<string>() { "TES-1", "TES-2", "TES-3" };
+            string user = ConfigurationManager.AppSettings[Constants.USER_KEY];
+            string passwd = ConfigurationManager.AppSettings[Constants.PASS_KEY];
+            string baseUrl = ConfigurationManager.AppSettings[Constants.BASEURL_KEY];
+
             foreach (string current in sources)
             {
-                Issue issue = GetJiraIssue(current);
+                Issue issue = JiraUtils.JiraMiscUtils.GetJiraIssueByKey(baseUrl, current, user, passwd);
                 Console.WriteLine("ID: {0}  KEY: {1}", issue.Id, issue.Key);
                 Console.WriteLine("Description: {0}",issue.Fields.Description);
                 Console.WriteLine("Created: {0}", issue.Fields.Created);
@@ -31,24 +35,6 @@ namespace JiraClient
             Console.Read();
         }
 
-        private static Issue GetJiraIssue(string key)
-        {
-            WebClient client = new WebClient();
-
-            // create credentials, base64 encode of username:password
-            string user = ConfigurationManager.AppSettings["user"];
-            string passwd = ConfigurationManager.AppSettings["pwd"];
-            string credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(string.Format("{0}:{1}", user, passwd)));
-            // Inject this string as the Authorization header
-            client.Headers[HttpRequestHeader.Authorization] = string.Format("Basic {0}", credentials);
-
-            string baseUrl = ConfigurationManager.AppSettings["baseurl"];
-            string url = string.Format("{0}/issue/{1}", baseUrl, key);
-
-            var jsonResponse = client.DownloadString(url);
-            Issue issue = JsonConvert.DeserializeObject<Issue>(jsonResponse);
-            return issue;
-
-        }
+      
     }
 }
---
+++ JiraClient/constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiraClient
+{
+    public static class Constants
+    {
+        public const string USER_KEY = "user";
+        public const string PASS_KEY = "pwd";
+        public const string BASEURL_KEY = "baseurl";
+    }
+}
