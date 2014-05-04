cef8f0285c9533a8ec97d1d70d1cabee468ed2a4
gardenunez <gardenunez@gmail.com>
2014-05-04 12:52:16 +0200

    Add new entities: watches, priority. Enrich IssueFields

--- JiraUtils/Entities/IssueFields.cs
+++ JiraUtils/Entities/IssueFields.cs
@@ -14,10 +14,17 @@ namespace JiraUtils.Entities
         public string Summary { get; set; }
         public string Description { get; set; }
 
+        public int? TimeSpent { get; set; }
+        public int? TimeEstimate { get; set; }
+        public int? TimeOriginalEstimate { get; set; }
+        public int? AggregateTimeSpent { get; set; }
+
         public IssueType IssueType { get; set; }
         public Project Project { get; set; }
         public IssueWorkLog Worklog { get; set; }
         public IssueComment Comment { get; set; }
+        public IssueWatches Watches {get;set;}
+        public Priority Priority { get; set; }
 
         public DateTime Created { get; set; }
         public DateTime Updated { get; set; }
---
+++ JiraUtils/Entities/IssueWatches.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JiraUtils.Entities
+{
+    public class IssueWatches
+    {
+        public string Self { get; set; }
+        public int WatchCount { get; set; }
+        public bool IsWatching { get; set; }
+    }
+}
