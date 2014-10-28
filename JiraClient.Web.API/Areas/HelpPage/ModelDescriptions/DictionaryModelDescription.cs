44da783262cbd989cdeb4c55b1c5884221be4b4b
jorge jardines <gardenunez@gmail.com>
2014-10-29 00:36:45 +0100

    delete default stuff created by VS

--- JiraClient.Web.API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace JiraClient.Web.API.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- JiraClient.Web.API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace JiraClient.Web.API.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
