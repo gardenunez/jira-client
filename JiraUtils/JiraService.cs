afa3b15b9d7a7fd6955b0d4665c3fd530f78ce8b
jorge jardines <gardenunez@gmail.com>
2014-11-02 21:43:00 +0100

    authentication when it is need

--- JiraUtils/JiraService.cs
+++ JiraUtils/JiraService.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -15,26 +16,6 @@ namespace JiraUtils
         public String ServerUrl { get; set; }
 
         /// <summary>
-        /// Get Jira Issue by key
-        /// </summary>
-        /// <param name="baseUrl"></param>
-        /// <param name="key"></param>
-        /// <param name="user"></param>
-        /// <param name="password"></param>
-        /// <returns></returns>
-        public Issue GetJiraIssueByKey(string key, string user, string password)
-        {
-            WebClient client = new WebClient();
-            string credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(string.Format("{0}:{1}", user, password)));
-            client.Headers[HttpRequestHeader.Authorization] = string.Format("Basic {0}", credentials);
-            string url = string.Format("{0}/issue/{1}", this.ServerUrl, key);
-            var jsonResponse = client.DownloadString(url);
-            Issue issue = JsonConvert.DeserializeObject<Issue>(jsonResponse);
-            return issue;
-
-        }
-
-        /// <summary>
         /// Get Jira Issue by Id or Key
         /// </summary>
         /// <param name="serverUrl"></param>
@@ -43,6 +24,13 @@ namespace JiraUtils
         public Issue GetJiraIssueByIdOrKey(string key)
         {
             WebClient client = new WebClient();
+            string user = ConfigurationManager.AppSettings["user"];
+            string password = ConfigurationManager.AppSettings["pwd"];
+            if (!string.IsNullOrEmpty(user) && !string.IsNullOrEmpty(password))
+            {
+                string credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(string.Format("{0}:{1}", user, password)));
+                client.Headers[HttpRequestHeader.Authorization] = string.Format("Basic {0}", credentials);
+            }
             string url = string.Format("{0}/issue/{1}", this.ServerUrl, key);
             var jsonResponse = client.DownloadString(url);
             Issue issue = JsonConvert.DeserializeObject<Issue>(jsonResponse);
